713e53ee18d9886cf4eaa1d8516bcaad4a462654
Natalia-doobro <79019782+Natalia-doobro@users.noreply.github.com>
2021-07-16 22:18:59 +0300

    задание 1

--- homework-07/src/js/task-01.js
+++ homework-07/src/js/task-01.js
@@ -1,1 +1,13 @@
-/*sss */
+const list = document.querySelector('#categories');
+console.log('Количество категорий: ', list.children.length);
+console.log(' ');
+
+const category = document.querySelectorAll('.item');
+console.log('Категория: ',category[0].firstElementChild.textContent);
+console.log('Количество элементов: ',category[0].lastElementChild.children.length);
+
+console.log('Категория: ',category[1].firstElementChild.textContent);
+console.log('Количество элементов: ', category[1].lastElementChild.children.length);
+
+console.log('Категория: ',category[2].firstElementChild.textContent);
+console.log('Количество элементов: ',category[2].lastElementChild.children.length);
